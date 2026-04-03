2348735347f52db0b460f81e4457b856b8cd61ec
AGmhwk <000991699@mohawkcollege.ca>
2026-04-03 15:29:53 -0400

    Update github.py

--- github.py
+++ github.py
@@ -9,12 +9,20 @@ while True:
     elif user_num < answer:
         print("Too low :(")
     else:
+        guess_count = 0
         print(f"Congrats you guessed it! It took you {guess_count} tries.")
-        break
+        again = input("would you like to try again? ")
+        if again.lower() == "yes":
+            answer = random.randint(1,1000)
+        elif again.lower() == "no":
+            print("thanks for playing")
+            break
+        else:
+            break
+            
     
 print("end")
-
+#Zohra
 def multiply(a, b):
     return a * b
 print(multiply(2, 3))
-
