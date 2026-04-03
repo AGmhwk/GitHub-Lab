6e9f97130503bf318cb50eae80b2923e4635ffcd
AGmhwk <000991699@mohawkcollege.ca>
2026-04-03 13:57:55 -0400

    A simple guessing game
    Guess a number from 1 to 1000

---
+++ github.py
@@ -0,0 +1,15 @@
+import random #getting random
+guess_count = 0 #amount of user guesses
+answer = random.randint(1,1000) #generating a random answer
+while True:
+    user_num = int(input("Guess a number from 1 to 1000 "))
+    guess_count += 1
+    if user_num > answer:
+        print("Too high!")
+    elif user_num < answer:
+        print("Too low :(")
+    else:
+        print(f"Congrats you guessed it! It took you {guess_count} tries.")
+        break
+    
+print("end")
